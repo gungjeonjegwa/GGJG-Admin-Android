edb7b2d4456cc8ee6301e0a2b68fd636f6cdcba0
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-03-01 15:36:16 +0900

    HttpHandler 변경

--- data/src/main/java/com/example/data/utils/HttpHandler.kt
+++ data/src/main/java/com/example/data/utils/HttpHandler.kt
@@ -1,21 +1,17 @@
 package com.example.data.utils
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 
-class HttpHandler<T> {
-    private lateinit var httpRequest: suspend () -> T
-    private var errorStatus: (code: Int, message: String) -> Throwable = {_, _ -> RuntimeException()}
-
-    fun httpRequest(httpRequest: suspend () -> T) =
-        this.apply { this.httpRequest = httpRequest }
-
-    fun onErrorStatus(errorStatus: (code: Int, message: String) -> Throwable) =
-        this.apply { this.errorStatus = errorStatus }
-
-    suspend fun sendRequest(): T =
-        try {
-            httpRequest()
-        } catch (e: HttpException) {
-            throw errorStatus(e.code(), e.message())
+suspend inline fun <T> GGJGApiCall(
+    crossinline callFunction: suspend () -> T,
+): T {
+    return try {
+        withContext(Dispatchers.IO) {
+            callFunction()
         }
+    } catch (e: HttpException) {
+        throw e
+    }
 }
